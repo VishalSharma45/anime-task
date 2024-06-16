22c1bc6f8cd1c850341b565095ef96bb6ea7a0f9
Vishal Sharma <invincible454@gmail.com>
2024-06-16 23:01:50 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import SearchPage from "./pages/SearchPage"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
+// import SearchPage from "./pages/SearchPage"
+const SearchPage = lazy(() => import("./pages/SearchPage"))
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SearchPage />} />
-        </Routes>
-      </BrowserRouter>
+      <Suspense fallback={<div className="flex items-center justify-center h-screen w-screen bg-indigo-100">
+        <h2 className=" text-5xl text-gray-700 font-semibold">Loading...</h2>
+      </div>}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<SearchPage />} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
     </>
   )
 }
